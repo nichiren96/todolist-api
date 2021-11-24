b3c65ba923a8c7314957c2eee3c39a39ce60d30b
Roel Tombozafy <rtombozafy96@gmail.com>
2021-11-24 18:50:06 +0300

    Setting environnement variable

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './.env'})
+
 const server = require('./src/app')
 
-const port = process.env.PORT 
+const port = process.env.PORT || 8000
 
 server.listen(port, () => console.log(`Server listening on port ${port}...`))
